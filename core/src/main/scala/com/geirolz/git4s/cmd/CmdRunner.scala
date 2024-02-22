cb52b0c96c5040054a371422e7a09a73e0558b62
geirolz <david.geirola@gmail.com>
2024-02-22 13:18:45 +0100

    Refactoring

--- core/src/main/scala/com/geirolz/git4s/cmd/CmdRunner.scala
+++ core/src/main/scala/com/geirolz/git4s/cmd/CmdRunner.scala
@@ -7,7 +7,7 @@ import com.geirolz.git4s.cmd.error.CmdFailure
 import com.geirolz.git4s.log.CmdLogger
 import fs2.io.file.Path
 import fs2.io.process.*
-import fs2.{text, Stream}
+import fs2.{Stream, text}
 
 trait CmdRunner[F[_]]:
   def stream[E, T](pd: Cmd[F, E, T])(using WorkingCtx, CmdLogger[F]): Stream[F, T]
@@ -21,58 +21,52 @@ private[git4s] object CmdRunner:
     new CmdRunner[F]:
 
       override def stream[E, T](cmd: Cmd[F, E, T])(using WorkingCtx, CmdLogger[F]): Stream[F, T] =
-        Stream
-          .resource(fp(cmd))
-          .evalMap((p: CmdProcess[F]) => {
-            for {
-              _        <- Async[F].unit
-              outTopic <- Queue.unbounded[F, String]
-              errTopic <- Queue.unbounded[F, String]
-
-              in: Stream[F, Nothing] =
+        for {
+          process  <- Stream.resource(fp(cmd))
+          outTopic <- Stream.eval(Queue.unbounded[F, Option[String]])
+          errTopic <- Stream.eval(Queue.unbounded[F, Option[String]])
+          out: Stream[F, T] =
+            process.stdout
+              .through(text.utf8.decode)
+              .evalTap(e => outTopic.offer(Some(e)))
+              .concurrently(
                 cmd.in
                   .through(text.utf8.encode)
-                  .through(p.stdin)
+                  .through(process.stdin)
+              )
+              .through(cmd.decoder.decode)
+              .rethrow
+              .onFinalize(outTopic.offer(None))
 
-              out: Stream[F, T] =
-                p.stdout
-                  .through(text.utf8.decode)
-                  .concurrently(in)
-                  .evalTap(outTopic.tryOffer(_).void)
-                  .debug()
-                  .through(cmd.decoder.decode)
-                  .rethrow
+          err: Stream[F, Nothing] =
+            process.stderr
+              .through(text.utf8.decode)
+              .evalTap(e => errTopic.offer(Some(e)))
+              .through(cmd.errorDecoder.decode)
+              .flatMap {
+                case Left(e)             => Stream.raiseError(e)
+                case Right(e: Throwable) => Stream.raiseError(e)
+                case Right(e: E)         => Stream.raiseError(CmdFailure(e.toString))
+              }
+              .onFinalize(errTopic.offer(None))
 
-              err: Stream[F, Nothing] =
-                p.stderr
-                  .through(text.utf8.decode)
-                  .evalTap(errTopic.tryOffer(_).void)
-                  .through(cmd.errorDecoder.decode)
-                  .flatMap {
-                    case Left(e)             => Stream.raiseError(e)
-                    case Right(e: Throwable) => Stream.raiseError(e)
-                    case Right(e: E)         => Stream.raiseError(CmdFailure(e.toString))
-                  }
-
-              logStream: Stream[F, Nothing] =
+          log: Stream[F, Nothing] =
+            Stream
+              .eval(process.exitValue)
+              .evalMap(exitCode => CmdLogger[F].log(cmd.compiled, cmd.in, exitCode).tupleLeft(exitCode))
+              .map {
+                case (0, logAction) => (outTopic, logAction)
+                case (_, logAction) => (errTopic, logAction)
+              }
+              .flatMap { case (queue, logAction) =>
                 Stream
-                  .eval(p.exitValue)
-                  .evalMap(exitCode => CmdLogger[F].log(cmd.compiled, cmd.in, exitCode).tupleLeft(exitCode))
-                  .map {
-                    case (0, logAction) => (outTopic, logAction)
-                    case (_, logAction) => (errTopic, logAction)
-                  }
-                  .flatMap { case (queue, logAction) =>
-                    Stream
-                      .eval(queue.tryTake)
-                      .repeat
-                      .unNoneTerminate
-                      .evalMap(logAction)
-                      .drain
-                  }
-            } yield out.merge(err).merge(logStream)
-          })
-          .flatten
+                  .fromQueueNoneTerminated(queue)
+                  .evalMap(logAction)
+                  .drain
+              }
+
+          result <- out.merge(err).onComplete(log)
+        } yield result
 
   given [F[_]: Processes](using F: Async[F]): CmdRunner[F] =
     CmdRunner.fromCmdProcess((cmd: Cmd[F, ?, ?]) => {
