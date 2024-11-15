cbf9bfe6917c89c5eb2c0b260f09dcfc48436536
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-11-15 19:00:23 +0000

    Update cats-effect to 3.5.5

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -5,7 +5,7 @@ object ProjectDependencies {
 
   // base
   private val catsVersion       = "2.12.0"
-  private val catsEffectVersion = "3.5.4"
+  private val catsEffectVersion = "3.5.5"
   private val fs2Version        = "3.10.2"
   // test
   private val munitVersion       = "1.0.0"
